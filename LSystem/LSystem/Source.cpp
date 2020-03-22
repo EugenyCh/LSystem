f9efa4163e2203558ecb2e2389fe739f91667a59
D:\Documents\Групповая разработка приложенийgit push --set-upstream https://gitlab.com/JurMarkin/simplebots.git master <eugeny007@gmail.com>
2020-03-22 16:54:40 +0300

    v0.0.2

--- LSystem/LSystem/Source.cpp
+++ LSystem/LSystem/Source.cpp
@@ -2,13 +2,93 @@
 #include "LSystem.h"
 #include <cmath>
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#define WIDTH 640
+#define HEIGHT 480
+
+vector<sf::Vertex> renderLSystem(string lsystem, double theta)
+{
+	vector<sf::Vertex> lines;
+	double angle = 0;
+	double h = 1.0;
+	double x, y;
+	x = y = 0;
+	double xmin, xmax, ymin, ymax;
+	xmin = xmax = 0;
+	ymin = ymax = 0;
+
+	for (int i = 0; i < lsystem.length(); ++i)
+	{
+		char c = lsystem[i];
+		switch (c)
+		{
+		case '+':
+			angle += theta;
+			break;
+		case '-':
+			angle -= theta;
+			break;
+		default:
+			lines.push_back(sf::Vertex(sf::Vector2f(x, y), sf::Color(127, 127, 127)));
+			x += h * cos(angle);
+			y += h * sin(angle);
+			lines.push_back(sf::Vertex(sf::Vector2f(x, y), sf::Color(255, 255, 255)));
+			if (x < xmin)
+				xmin = x;
+			if (x > xmax)
+				xmax = x;
+			if (y < ymin)
+				ymin = y;
+			if (y > ymax)
+				ymax = y;
+		}
+	}
+
+	double scale_x = WIDTH / (xmax - xmin);
+	double scale_y = HEIGHT / (ymax - ymin);
+	double x0 = (xmax + xmin) / 2;
+	double y0 = (ymax + ymin) / 2;
+
+	for (int i = 0; i < lines.size(); ++i)
+	{
+		lines[i].position.x *= scale_x;
+		lines[i].position.x -= x0;
+		lines[i].position.x += WIDTH / 2;
+		lines[i].position.y *= scale_y;
+		lines[i].position.y -= y0;
+		lines[i].position.y += HEIGHT / 2;
+	}
+
+	return lines;
+}
 
 int main()
 {
-	auto lsystem = LSystem("X");
-	lsystem.set_rule('X', "X+Y+");
-	lsystem.set_rule('Y', "-X-Y");
-	cout << lsystem.iterate(3) << endl;
+	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "LSystem Window");
+	window.setFramerateLimit(2);
+
+	vector<sf::Vertex> lines;
+	lines = renderLSystem("F++F++F", M_PI / 3);
+	sf::Vertex* vertices = lines.data();
+	size_t vlen = lines.size();
+
+	for (int i = 0; i < vlen; ++i)
+	{
+		cout << "(" << vertices[i].position.x << ", " << vertices[i].position.y << ")" << endl;
+	}
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		window.clear();
+		window.draw(vertices, vlen, sf::Lines);
+		window.display();
+	}
 
 	return 0;
 }
