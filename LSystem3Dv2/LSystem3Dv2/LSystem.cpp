b5f975b87a67e0507805f821169b34ad615f97e6
D:\Documents\Групповая разработка приложенийgit push --set-upstream https://gitlab.com/JurMarkin/simplebots.git master <eugeny007@gmail.com>
2020-04-19 16:35:30 +0300

    Two commit

--- LSystem3Dv2/LSystem3Dv2/LSystem.cpp
+++ LSystem3Dv2/LSystem3Dv2/LSystem.cpp
@@ -1,128 +1,128 @@
-#include	<stdio.h>
-#include	<GL/glut.h>
-#include	"LSystem.h"
+#include <stdio.h>
+#include <GL/glut.h>
+#include "LSystem.h"
 
 LSystem::LSystem()
 {
-	initialString = "F";
-	angle = 30 * M_PI / 180;
-	distScale = 1;
-	angleScale = 1;
+    initialString = "F";
+    angle = 30 * M_PI / 180;
+    distScale = 1;
+    angleScale = 1;
 }
 
-void	LSystem::interpretString(const string& str)
+void LSystem::interpretString(const string& str)
 {
-	State			state;
-	stack<State>	st;
-	Vector3D		d;
+    State state;
+    stack<State> st;
+    Vector3D d;
 
-	state.pos = Vector3D(0, 0, 0);
-	state.dir = Vector3D(1, 0, 0);
-	state.angles = Vector3D(0, 0, 0);
-	state.angle = angle;
-	state.invert = 1;
+    state.pos = Vector3D(0, 0, 0);
+    state.dir = Vector3D(1, 0, 0);
+    state.angles = Vector3D(0, 0, 0);
+    state.angle = angle;
+    state.invert = 1;
 
-	for (int i = 0; i < str.length(); i++)
-	{
-		switch (str[i])
-		{
-		case '[':		// push state
-			st.push(state);
-			break;
+    for (int i = 0; i < str.length(); i++)
+    {
+        switch (str[i])
+        {
+        case '[': // push state
+            st.push(state);
+            break;
 
-		case ']':		// pop state
-			state = st.top();
-			st.pop();
-			break;
+        case ']': // pop state
+            state = st.top();
+            st.pop();
+            break;
 
-		case 'F':
-			d = step(state);
-			bounds.addVertex(state.pos);
-			bounds.addVertex(state.pos + d);
-			drawLine(state.pos, state.pos + d, st.size());
-			updateState(state, d, st.size());
-			break;
+        case 'F':
+            d = step(state);
+            bounds.addVertex(state.pos);
+            bounds.addVertex(state.pos + d);
+            drawLine(state.pos, state.pos + d, st.size());
+            updateState(state, d, st.size());
+            break;
 
-		case 'f':
-			bounds.addVertex(state.pos);
-			bounds.addVertex(state.pos + d);
-			updateState(state, step(state), st.size());
-			break;
+        case 'f':
+            bounds.addVertex(state.pos);
+            bounds.addVertex(state.pos + d);
+            updateState(state, step(state), st.size());
+            break;
 
-		case '!':						// inverse + and - meaing (just as for & and ^ and < and >)
-			state.invert *= -1;
-			break;
+        case '!': // inverse + and - meaing (just as for & and ^ and < and >)
+            state.invert *= -1;
+            break;
 
-		case '+':
-			state.angles.z += state.invert * state.angle;
-			break;
+        case '+':
+            state.angles.z += state.invert * state.angle;
+            break;
 
-		case '-':
-			state.angles.z -= state.invert * state.angle;
-			break;
+        case '-':
+            state.angles.z -= state.invert * state.angle;
+            break;
 
-		case '&':
-			state.angles.x += state.invert * state.angle;
-			break;
+        case '&':
+            state.angles.x += state.invert * state.angle;
+            break;
 
-		case '^':
-			state.angles.x -= state.invert * state.angle;
-			break;
+        case '^':
+            state.angles.x -= state.invert * state.angle;
+            break;
 
-		case '<':
-			state.angles.y += state.invert * state.angle;
-			break;
+        case '<':
+            state.angles.y += state.invert * state.angle;
+            break;
 
-		case '>':
-			state.angles.y -= state.invert * state.angle;
-			break;
-		}
-	}
+        case '>':
+            state.angles.y -= state.invert * state.angle;
+            break;
+        }
+    }
 }
 
-void	LSystem::buildSystem(int numIterations)
+void LSystem::buildSystem(int numIterations)
 {
-	bounds.reset();
+    bounds.reset();
 
-	currentString = initialString;
+    currentString = initialString;
 
-	for (int i = 0; i < numIterations; i++)
-	{
-		currentString = oneStep(currentString);
+    for (int i = 0; i < numIterations; i++)
+    {
+        currentString = oneStep(currentString);
 
-		printf("%s\n", currentString.c_str());
-	}
+        printf("%s\n", currentString.c_str());
+    }
 }
 
-string	LSystem::oneStep(const string& in) const
+string LSystem::oneStep(const string& in) const
 {
-	string						   		out;
-	map <char, string> ::const_iterator it;
+    string out;
+    map<char, string>::const_iterator it;
 
-	for (int i = 0; i < in.length(); i++)
-	{
-		it = rules.find(in[i]);
+    for (int i = 0; i < in.length(); i++)
+    {
+        it = rules.find(in[i]);
 
-		if (it != rules.end())
-			out += it->second;
-		else
-			out += in[i];
-	}
+        if (it != rules.end())
+            out += it->second;
+        else
+            out += in[i];
+    }
 
-	return out;
+    return out;
 }
 
-void	LSystem::drawLine(const Vector3D& p1, const Vector3D& p2, int level) const
+void LSystem::drawLine(const Vector3D& p1, const Vector3D& p2, int level) const
 {
-	glBegin(GL_LINES);
-	glVertex3fv(p1);
-	glVertex3fv(p2);
-	glEnd();
+    glBegin(GL_LINES);
+    glVertex3fv(p1);
+    glVertex3fv(p2);
+    glEnd();
 }
 
-void	LSystem::updateState(State& state, const Vector3D& dir, int level) const
+void LSystem::updateState(State& state, const Vector3D& dir, int level) const
 {
-	state.pos += dir;
-	state.dir *= distScale;
-	state.angle *= angleScale;
+    state.pos += dir;
+    state.dir *= distScale;
+    state.angle *= angleScale;
 }
--- LSystem3Dv2/LSystem3Dv2/LSystem.h
+++ LSystem3Dv2/LSystem3Dv2/LSystem.h
@@ -10,79 +10,79 @@
 
 using namespace std;
 
-class	LSystem
+class LSystem
 {
-	struct	State
-	{
-		Vector3D pos;
-		Vector3D dir;
-		Vector3D angles;
-		float    angle;
-		float    invert;
-	};
+    struct State
+    {
+        Vector3D pos;
+        Vector3D dir;
+        Vector3D angles;
+        float angle;
+        float invert;
+    };
 
-	map <char, string> rules;
-	string             initialString;
-	float              angle;
-	float              distScale;
-	float              angleScale;
-	string             currentString;
-	BoundingBox        bounds;
+    map<char, string> rules;
+    string initialString;
+    float angle;
+    float distScale;
+    float angleScale;
+    string currentString;
+    BoundingBox bounds;
 
 public:
-	LSystem();
+    LSystem();
 
-	void setInitialString(const char* str)
-	{
-		initialString = str;
-	}
+    void setInitialString(const char* str)
+    {
+        initialString = str;
+    }
 
-	void addRule(char symbol, const char* rule)
-	{
-		rules[symbol] = rule;
-	}
+    void addRule(char symbol, const char* rule)
+    {
+        rules[symbol] = rule;
+    }
 
-	void setAngle(float newAngle)
-	{
-		angle = newAngle;
-	}
+    void setAngle(float newAngle)
+    {
+        angle = newAngle;
+    }
 
-	const string& getCurrentString() const
-	{
-		return currentString;
-	}
+    const string& getCurrentString() const
+    {
+        return currentString;
+    }
 
-	void setDistScale(float newScale)
-	{
-		distScale = newScale;
-	}
+    void setDistScale(float newScale)
+    {
+        distScale = newScale;
+    }
 
-	void setAngleScale(float scale)
-	{
-		angleScale = scale;
-	}
+    void setAngleScale(float scale)
+    {
+        angleScale = scale;
+    }
 
-	void interpretString(const string& str);
-	void buildSystem(int numIterations);
+    void interpretString(const string& str);
+    void buildSystem(int numIterations);
 
-	void draw()
-	{
-		interpretString(currentString);
-	}
+    void draw()
+    {
+        interpretString(currentString);
+    }
 
-	const BoundingBox& getBounds() const
-	{
-		return bounds;
-	}
+    const BoundingBox& getBounds() const
+    {
+        return bounds;
+    }
 
 protected:
-	string   oneStep(const string& in) const;
-	Vector3D step(const State& state) const
-	{
-		return Matrix3D::rotateZ(state.angles.z) * Matrix3D::rotateY(state.angles.y) *
-			Matrix3D::rotateX(state.angles.x) * state.dir;
-	}
+    string oneStep(const string& in) const;
+    Vector3D step(const State& state) const
+    {
+        return Matrix3D::rotateZ(state.angles.z) * Matrix3D::rotateY(state.angles.y) *
+            Matrix3D::rotateX(state.angles.x) * state.dir;
+    }
 
-	virtual void drawLine(const Vector3D& p1, const Vector3D& p2, int level) const;
-	virtual void updateState(State& state, const Vector3D& dir, int level) const;
+    virtual void drawLine(const Vector3D& p1, const Vector3D& p2, int level) const;
+    virtual void updateState(State& state, const Vector3D& dir, int level) const;
 };
