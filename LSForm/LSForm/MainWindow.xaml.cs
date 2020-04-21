f72976abf9e5d163ff222218aabc41e32cff8596
D:\Documents\Групповая разработка приложенийgit push --set-upstream https://gitlab.com/JurMarkin/simplebots.git master <eugeny007@gmail.com>
2020-04-21 19:22:23 +0300

    Form creation

---
+++ LSForm/LSForm/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LSForm
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var TopMenuItems = new ObservableCollection<MenuItem>
+            {
+                new MenuItem
+                {
+                    Header = "File",
+                    Items = {
+                        new MenuItem
+                        {
+                            Header = "New"
+                        },
+                        new MenuItem
+                        {
+                            Header = "Open"
+                        },
+                        new MenuItem
+                        {
+                            Header = "Save"
+                        }
+                    }
+                },
+                new MenuItem
+                {
+                    Header = "Edit"
+                },
+                new MenuItem
+                {
+                    Header = "Search"
+                }
+            };
+
+            ReadyTemplates.ItemsSource = TopMenuItems;
+        }
+    }
+}
