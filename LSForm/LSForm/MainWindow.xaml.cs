ae5a00e16f43bb1da102c3ad3c51ce1379377589
D:\Documents\Групповая разработка приложенийgit push --set-upstream https://gitlab.com/JurMarkin/simplebots.git master <eugeny007@gmail.com>
2020-04-22 18:06:56 +0300

    Button +

--- LSForm/LSForm/MainWindow.xaml.cs
+++ LSForm/LSForm/MainWindow.xaml.cs
@@ -28,5 +28,28 @@ namespace LSForm
             InitializeComponent();
 
         }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Grid grid = new Grid();
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(24.0) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(24.0) });
+            grid.Margin = new Thickness(0.0, 0.0, 0.0, 8.0);
+            TextBox boxR = new TextBox();
+            boxR.Text = "R";
+            Grid.SetColumn(boxR, 0);
+            TextBox boxE = new TextBox();
+            boxE.Margin = new Thickness(8.0, 0.0, 8.0, 0.0);
+            boxE.Text = $"Rule #{StackRules.Children.Count}";
+            Grid.SetColumn(boxE, 1);
+            Button button = new Button();
+            button.Content = "-";
+            Grid.SetColumn(button, 2);
+            grid.Children.Add(boxR);
+            grid.Children.Add(boxE);
+            grid.Children.Add(button);
+            StackRules.Children.Insert(StackRules.Children.Count - 1, grid);
+        }
     }
 }
