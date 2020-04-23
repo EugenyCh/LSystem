2540d845659dff9403c4232c026348c17909bc63
D:\Documents\Групповая разработка приложенийgit push --set-upstream https://gitlab.com/JurMarkin/simplebots.git master <eugeny007@gmail.com>
2020-04-23 16:20:02 +0300

    Reader end

--- LSBinaryReader/LSBinaryReader/LSBinaryReader.cpp
+++ LSBinaryReader/LSBinaryReader/LSBinaryReader.cpp
@@ -2,6 +2,26 @@
 #include <fstream>
 #include <map>
 
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
+#define COLORED(v) BOLDGREEN << v << RESET
+
 using namespace std;
 
 int main()
@@ -16,6 +36,9 @@ int main()
 	string initString;
 	int rulesCount;
 	map<char, string> rules;
+	int startIter;
+	float width0, width1;
+	unsigned char color0[4], color1[4];
 	int temp;
 	char* strTemp;
 
@@ -47,13 +70,41 @@ int main()
 		delete[] strTemp;
 	}
 
+	// Start iteration
+	in.read((char*)&startIter, sizeof(int));
+
+	// Color 0
+	in.read((char*)&color0, 4);
+
+	// Color 1
+	in.read((char*)&color1, 4);
+
+	// Width 0
+	in.read((char*)&width0, sizeof(float));
+
+	// Width 1
+	in.read((char*)&width1, sizeof(float));
+
 	in.close();
-	cout << "Rules count : " << rulesCount << endl;
-	cout << "Init string (" << temp << " bytes) : " << initString << endl;
+	cout << "Rules count : " << COLORED(rulesCount) << endl;
+	cout << "Init string (" << COLORED(initString.length()) << " bytes) : " << COLORED(initString) << endl;
 	for (auto r : rules)
 	{
-		cout << "Rule '" << r.first << "' (" << r.second.length() << ") : " << r.second << endl;
+		cout << "Rule '" << COLORED(r.first) << "' (" << COLORED(r.second.length()) << " bytes) : " << COLORED(r.second) << endl;
 	}
+	cout << "Start iteration : " << COLORED(startIter) << endl;
+	cout << "Color 0 : " << COLORED("[")
+		<< COLORED((int)(color0[0])) << COLORED(", ")
+		<< COLORED((int)(color0[1])) << COLORED(", ")
+		<< COLORED((int)(color0[2])) << COLORED(", ")
+		<< COLORED((int)(color0[3])) << COLORED("]") << endl;
+	cout << "Color 1 : " << COLORED("[")
+		<< COLORED((int)(color1[0])) << COLORED(", ")
+		<< COLORED((int)(color1[1])) << COLORED(", ")
+		<< COLORED((int)(color1[2])) << COLORED(", ")
+		<< COLORED((int)(color1[3])) << COLORED("]") << endl;
+	cout << "Width 0 : " << COLORED(width0) << endl;
+	cout << "Width 1 : " << COLORED(width1) << endl;
 
 	return 0;
 }
