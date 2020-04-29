6e6ea8d6ac390702815eb642429bad734e94df58
D:\Documents\Групповая разработка приложенийgit push --set-upstream https://gitlab.com/JurMarkin/simplebots.git master <eugeny007@gmail.com>
2020-04-29 14:58:12 +0300

    MatLab begin

--- MatLab/main.m
+++ MatLab/main.m
@@ -26,14 +26,29 @@ R = @(a) Ru(a(1)) * Rl(a(2)) * Rh(a(3));
 vs = [v0'];
 s = '^\XF^\XFX-F^//XFX&F+//XFX-F/X-/';
 angs = [0 0 0];
+phi = 0.5 * pi;
 for ns = 1:length(s)
     switch s(ns)
         case 'F'
             vs = [vs; (R(angs) * v0)'];
+        case '+'
+            angs(1) = angs(1) + phi;
+        case '-'
+            angs(1) = angs(1) - phi;
+        case '&'
+            angs(2) = angs(2) + phi;
+        case '^'
+            angs(2) = angs(2) - phi;
+        case '\'
+            angs(3) = angs(3) + phi;
+        case '/'
+            angs(3) = angs(3) - phi;
     end
 end
-% xs = [0 0];
-% ys = [0 1];
-% zs = [0 1];
-% plot3(xs, ys, zs)
-% rotate3d
+
+vst = vs';
+xs = vst(1,:);
+ys = vst(2,:);
+zs = vst(3,:);
+plot3(xs, ys, zs)
+rotate3d
